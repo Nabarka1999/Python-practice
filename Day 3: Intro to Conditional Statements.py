e17d3ca8bd87315886a3c5452241dff2bc95f83f
Nabarka1999 <82209383+Nabarka1999@users.noreply.github.com>
2021-08-03 22:35:26 +0530

    Create Day 3: Intro to Conditional Statements.py

---
+++ Day 3: Intro to Conditional Statements.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+if (N%2==0):
+    if (N>=2) and (N<=5):
+        print('Not Weird')
+    if (N>=6) and (N<=20):
+        print('Weird')
+    if (N>20):
+        print('Not Weird')
+else: 
+    print('Weird')
